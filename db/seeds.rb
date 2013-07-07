8488f5746dcf945b75c7faecf777365432106306
--WillMcVay <--willmcvay@gmail.com>
2013-07-07 18:32:39 +0100

    re-added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.create! name: 'Lasagne', steps: 'Do Some Stuff', food_type: 'Italian'
+Picture.create! pictures: 'spag_bol.jpg'
+Ingredient.create! ingredients: 'Meat, tomatoes'
