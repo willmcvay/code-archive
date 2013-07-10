2726b2a8e0111665206d8f3a104088a106465a87
--WillMcVay <--willmcvay@gmail.com>
2013-07-10 09:05:19 +0100

    added cancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
   def index
     @users = User.all
   end
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_path
+      redirect_to users_index
     else
       render 'new'
     end
