e50daa1e691799340da258d8a75c908eb4935741
willmcvay <willmcvay@gmail.com>
2013-07-14 16:00:09 +0100

    set up controllers, db, initial migration

---
+++ app/helpers/mixs_helper.rb
@@ -0,0 +1,2 @@
+module MixsHelper
+end
---
+++ db/migrate/20130714144200_create_mixes.rb
@@ -0,0 +1,13 @@
+class CreateMixes < ActiveRecord::Migration
+  def change
+    create_table :mixes do |t|
+      t.string :djname
+      t.string :mix
+      t.string :genre
+      t.string :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
