1f77ecf7230ace688270a5453a464d9686755f64
--WillMcVay <--willmcvay@gmail.com>
2013-07-14 20:11:33 +0100

    routes all working

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ActiveRecord::Base
-  attr_accessible :biog, :djname, :facebook, :image, :location, :soundcloud, :twitter, :wallpaper
+  attr_accessible :biog, :djname, :facebook, :image, :location, :soundcloud, :twitter, :wallpaper, :mix_ids, :comment_ids
+  has_many :comment_ids, :mix_ids
 end
