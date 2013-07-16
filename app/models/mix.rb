ec5a152106e0e7e3d2cb5e2693ac6fa1cbead3a1
willmcvay <willmcvay@gmail.com>
2013-07-16 09:43:14 +0100

    uploader for mixes and media player working, css fix

--- app/models/mix.rb
+++ app/models/mix.rb
@@ -1,4 +1,6 @@
 class Mix < ActiveRecord::Base
+  mount_uploader :mix, MixUploader
+  mount_uploader :image, ImageUploader
   attr_accessible :description, :djname, :genre, :image, :mix, :artist_id, :comment_id
   belongs_to :artist
   has_many :comments
