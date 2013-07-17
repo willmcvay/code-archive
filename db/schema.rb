77178a6fbc8fed8943f7995377de0f797d21dc97
willmcvay <willmcvay@gmail.com>
2013-07-18 00:51:24 +0100

    bug free, more styling done, cancan gem to add

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130717085430) do
+ActiveRecord::Schema.define(:version => 20130717230859) do
 
   create_table "artists", :force => true do |t|
     t.string   "djname"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(:version => 20130717085430) do
     t.string   "facebook"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "mix_id"
+    t.string   "genre"
   end
 
   create_table "comments", :force => true do |t|
@@ -43,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130717085430) do
     t.string   "image"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "mixname"
   end
 
   create_table "users", :force => true do |t|
