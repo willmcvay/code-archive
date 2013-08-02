03140d49e2d0af1e0b3adb738749f1e9a3f3eb12
--WillMcVay <--willmcvay@gmail.com>
2013-08-02 13:57:50 +0100

    basic devise login added, home controller, hompage view added. New User saves to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :feeds, through: :feed_user
   has_many :entries, through: :entry_user
 
-  attr_accessible :bio, :email, :first_name, :last_name, :photo, :role
+  attr_accessible :bio, :email, :first_name, :last_name, :photo, :role, :password, :password_confirmation, :remember_me
 end
