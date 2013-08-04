6ecf12f691d0435254bc228a57f399870b99d187
Stewart Scougall <stewart@scougs.com>
2013-08-04 20:30:28 +0100

    Updated sidebar layout and css styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rssreader::Application.routes.draw do
 
+  devise_for :users
+
   resources :feeds
   post '/feeds', to: 'feeds#create', as: 'create_feed'
-
 
   resources :posts
   resources :users
 
-  devise_for :users
-
   get '/my_profile', to: 'users#my_profile', as: :my_profile
-  get "home/index"
  
   root to: "home#home"
 
