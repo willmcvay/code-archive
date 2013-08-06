973cf7284333bb741a95d98dcb183c445bc3374c
--WillMcVay <--willmcvay@gmail.com>
2013-08-06 12:40:20 +0100

    merge local branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,9 +53,7 @@ class UsersController < ApplicationController
 
     end
 
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+
 
     def my_profile
       @user = current_user
