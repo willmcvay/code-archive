c9b883788caf829caf48001f8b1ebd42c5535f39
willmcvay <willmcvay@gmail.com>
2013-08-24 11:02:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandsite::Application.config.secret_token = 'a8effdcac9c5c7d1c7da635270a5b17c2e10dd5517592670a1c8f2d5e989d8e36948c56c7671a62a297fd10c0bb199a38a37909fa5be8da86ff27096f4818ea3'
