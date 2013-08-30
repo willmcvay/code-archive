2d5febacdd911bfed1d2291d122476d2789fa4f8
willmcvay <willmcvay@gmail.com>
2013-08-30 13:34:11 +0100

    added jquery comments to posts

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,11 @@
 class PostCommentsController < ApplicationController
 
   def create
-    @postComment = PostComment.create(params[:post_comment])
-    redirect_to post_path(params[:post_id])
+    @post_comment = PostComment.create(params[:post_comment])
+      respond_to do |format|  
+        @post_comment.save  
+        # format.html { redirect_to(post_path(params[:post_id]) }  
+        format.js  
+    end
   end
 end
