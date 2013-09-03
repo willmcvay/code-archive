fabb84f7ba8a0a10ee3caace84ea9a349154d6f6
Will McVay <willmcvay@gmail.com>
2013-09-03 14:23:46 +0100

    mandrill setting tweak

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,7 @@ Bandsite::Application.configure do
     :port => 587, 
     :user_name => ENV["MANDRILL_USERNAME"], 
     :password => ENV["MANDRILL_APIKEY"],
-    :enable_starttls_auto => true, 
-    :domain => "heroku.com"
+    :enable_starttls_auto => true
 }
 
 ActionMailer::Base.delivery_method = :smtp
