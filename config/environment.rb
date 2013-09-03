2ba441c0efc6d643910c3f1cfd51a26ba1e699d6
willmcvay <willmcvay@gmail.com>
2013-09-03 10:09:30 +0100

    Production config & remove uploads from git

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Bandsite::Application.initialize!
-
-
-aws_access_key_id = "AKIAILLBP5HQYCE5HDOA"
-aws_secret_access_key = "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"
-mandrill_password = "LxHgZJFCdFjatoWcKnY70w"
