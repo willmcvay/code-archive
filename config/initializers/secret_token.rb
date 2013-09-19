fd8c1101ccf02e65c270f0f0bbf299aeaacb008d
willmcvay <willmcvay@gmail.com>
2013-09-19 13:08:24 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitapi::Application.config.secret_token = '7def7a0daeb5d36e518c1b3fa05772257bcdbc7617d86b929b4807df4209596df762bc961fea26a5fbec7ab3c7380e03bb777c0a3ab37dffefd0658d6af22fc1'
