55a89a0f68e702484cb1eed42be17037d0a30182
Will McVay <willmcvay@gmail.com>
2014-01-31 17:53:17 +0000

    Some final styling to do

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ var randomDeck = [];
 var domDivsParent = null;
 var cardOne = {};
 var cardTwo = {};
-var cardsSelected = false;
 var eventsAssigned = false;
 var playerOne = true;
 var playerTwo = false;
+var loadStatusCalls = 0;
+
+function hideLoadPage() {
+
+	var loadingPage = document.getElementById("loading-page");
+	var controls = document.getElementById("controls");
+	controls.classList.remove('hide');
+	loadingPage.className += ('hide');
+}
+
+function checkLoadStatus (deckToDeal) {
+	var tempArray = [];
+	tempArray.push(deckToDeal);
+	loadStatusCalls++;
+	if (tempArray.length === loadStatusCalls)  {
+		setTimeout(function(){
+			hideLoadPage();
+			console.log("hiding load page");
+		},2000)	
+	}
+}
 
 function makeDeck () {
+
 	for (var j = 0; j < cardSuitsArray.length; j++) {
 		var selectedSuit = cardSuitsArray[j]
 		for (var i = 0; i < cardValuesArray.length; i++) {
@@ -26,6 +47,7 @@ function makeDeck () {
 }
 
 function shuffleDeck() {
+
 	var shuffled = deck.slice(0), i = deck.length, temp, index; while (i--) {
         		index = Math.floor((i + 1) * Math.random());
         		temp = shuffled[index];
@@ -36,8 +58,15 @@ function shuffleDeck() {
 }
 
 function pressToDeal(){
+	
 	var pressButton = document.getElementById("deal-deck");
+	var playAgain = document.getElementById("play-again");
+	
 	pressButton.addEventListener("click", function() {
+	window.location.reload();
+	}, false);
+
+	playAgain.addEventListener("click", function() {
 	window.location.reload();
 	}, false);
 }
@@ -53,6 +82,7 @@ function dealDeck () {
 		deckToDeal.id = "card" + i;
 		deckToDeal.innerHTML += "<p> " + randomDeck[i].cardValue + " " + randomDeck[i].cardSuit + "</p>";
 		domDivsParent.appendChild(deckToDeal);
+		domDivsParent.addEventListener("load", checkLoadStatus(deckToDeal), false);
 	};
 	document.getElementById("container").appendChild(domDivsParent);
 }
@@ -72,22 +102,59 @@ function selectCards(clickedCard) {
 
 	if (cardOne.cardValue === undefined && clickedCard !== undefined) {
 		cardOne.cardValue = clickedCard.innerHTML.split(" ")[1];
+		cardOne.cardSuit = clickedCard.innerHTML.split(" ")[2];
+		cardOne.id = clickedCard.id;
 		cardOne.element = clickedCard;
-		cardOne.element.className += " flip-card";
+		cardOne.selected = true;
+
+		if (cardOne.cardSuit === "Hearts</p>") {
+			cardOne.element.className += " heart flip-card";
+		} else if (cardOne.cardSuit === "Clubs</p>") {
+			cardOne.element.className += " club flip-card";
+		} else if (cardOne.cardSuit === "Diamonds</p>") {
+			cardOne.element.className += " diamond flip-card";
+		} else if (cardOne.cardSuit === "Spades</p>") {
+			cardOne.element.className += " spade flip-card";
+		}
 		console.log("First Card Selected: " + cardOne.cardValue);
 
 	} else if (cardTwo.cardValue === undefined && clickedCard !== undefined)  {
 		cardTwo.cardValue = clickedCard.innerHTML.split(" ")[1];
+		cardTwo.cardSuit = clickedCard.innerHTML.split(" ")[2];
+		cardTwo.id = clickedCard.id;
+		cardTwo.selected = true;
 		cardTwo.element = clickedCard;
-		cardTwo.element.className += " flip-card";
+
+		if (cardTwo.cardSuit === "Hearts</p>") {
+			cardTwo.element.className += " heart flip-card";
+		} else if (cardTwo.cardSuit === "Clubs</p>") {
+			cardTwo.element.className += " club flip-card";
+		} else if (cardTwo.cardSuit === "Diamonds</p>") {
+			cardTwo.element.className += " diamond flip-card";
+		} else if (cardTwo.cardSuit === "Spades</p>") {
+			cardTwo.element.className += " spade flip-card";
+		}
 		console.log("Both Cards selected: " + cardOne.cardValue + " " + cardTwo.cardValue);
-		cardsSelected = true;
-	}
-	
-	if (cardsSelected === true) {
+	} 
+checkCardsUnique();
+}
+
+function checkCardsUnique () {
+
+	 if (cardOne.id === cardTwo.id){
+		cardTwo.cardValue = undefined;
+		cardTwo.id = undefined;
+		cardTwo.selected = undefined;
+		document.getElementById("duplicate-alert").classList.remove("hide");
+		selectCards();
+		setTimeout(function(){
+			document.getElementById("duplicate-alert").className += "hide";	
+		},2000)
+
+	} else if (cardOne.selected === true && cardTwo.selected === true && cardOne.id !== cardTwo.id ) {
 		setTimeout(function(){
 			playGame();
-		},3000)
+		},2000)
 	}
 }
 
@@ -96,13 +163,14 @@ function playGame () {
 	var scoreOne = document.getElementById("player-one-score");
 	var scoreTwo = document.getElementById("player-two-score");
 
-	if (cardsSelected === true && cardOne.cardValue === cardTwo.cardValue) {
+	if (cardOne.selected === true && cardTwo.selected === true && cardOne.cardValue === cardTwo.cardValue) {
 		console.log("Cards will destroy");
 		cardOne.element.className += " destroy-card";
 		cardTwo.element.className += " destroy-card";
 		cardOne.cardValue = undefined;
 		cardTwo.cardValue = undefined;
-		cardsSelected = false;
+		cardOne.selected = false;
+		cardTwo.selected = false;
 		
 		if (playerOne === true) {
 			scoreOne.innerHTML++;
@@ -113,7 +181,8 @@ function playGame () {
 
 	} else {
 		console.log("Cards will do nothing");
-		cardsSelected = false;
+		cardOne.selected = false;
+		cardTwo.selected = false;
 		cardOne.element.classList.remove("flip-card");
 		cardTwo.element.classList.remove("flip-card");
 		cardOne.cardValue = undefined;
@@ -146,10 +215,25 @@ function playGame () {
 	}
 
 	var allCards = document.getElementsByClassName("card");
+// needed to test Game over screen
+	// var allCards = [];
 
 	if (allCards.length > 0) {
 		console.log("Play On");
 	} else {
+		document.getElementById("game-over").classList.remove("hide");
+
+		var p1 = "Player 1";
+		var p2 = "Player 2";
+		var winner = document.createElement('h2');
+
+		if (scoreOne.innerHTML >= scoreTwo.innerHTML) {
+			winner.innerHTML += p1 + " is the Winner, with " + scoreOne.innerHTML + " points!";
+		} else if (scoreOne.innerHTML < scoreTwo.innerHTML) {
+			winner.innerHTML += p2 + " is the Winner, with " + scoreTwo.innerHTML + " points!";
+		}	
+		document.getElementById("game-over").appendChild(winner);
+
 		console.log("Game Over");
 	}
 }
