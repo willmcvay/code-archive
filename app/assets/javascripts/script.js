accd9df8004722d3f701b142356cc8693264ff53
Will McVay <willmcvay@gmail.com>
2014-04-20 13:19:16 +0100

    basic slide transition working

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,18 +1,17 @@
 // Globals object
 
 var GLOBALS = {};
-GLOBALS.pageHeight = window.innerHeight;
-GLOBALS.pageWidth = window.innerWidth;
-transform = "webkitTransform" || "mozTransform";
-transition = "webkitTransition" || "mozTansition";
-transEnd = "webkitTransitionEnd"
+var pageHeight = window.innerHeight;
+var pageWidth = window.innerWidth;
+var transform = "webkitTransform" || "mozTransform";
+var transition = "webkitTransition" || "mozTansition";
+var transEnd = "webkitTransitionEnd"
 
 $(document).ready(function(){
 
 // homepage slides
-
-	var styles = {
-      		transform : "translate3d(0, 1000px, 0)",
+	var transStyles = {
+      		transform : "translate3d(0," + pageHeight + "px, 0)",
       		transition : "all 1s ease-in-out"
    	 };
 	
@@ -23,9 +22,11 @@ $(document).ready(function(){
 			$('.homepage-slide').each(function(i, v){
 				console.log(i, v);
 				
-				$('#slide-' + i).css( styles );			
+				$('#slide-' + i).css( transStyles );			
 				$('#slide-' + i).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
-					$('#slide-' + i).attr("style","");  
+					$('#slide-' + i).css( "top", "+=" + pageHeight );
+					$('#slide-' + i).css( transform, "");
+					$('#slide-' + i).css( transition, "");
 				})
 			})
 		})
