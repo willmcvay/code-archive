b00d562d8af54b32c0337f74628d38fbd00c82ea
Will McVay <willmcvay@gmail.com>
2014-04-21 10:45:03 +0100

    Fixed access to admin area; added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.create! :name => 'Me', :email => 'me@example.com', :password => 'password', :password_confirmation => 'password'
