326fcbd7fcaf6264d9eb30ef2cb66cc098ea9812
Will McVay <willmcvay@gmail.com>
2014-09-08 22:45:05 +0100

    Set up basic game boilerplate

---
+++ constants.js
@@ -0,0 +1,67 @@
+var playerOne = {
+	score: 0
+}
+
+var playerTwo = {
+	score: 0
+}
+
+var tiles = [
+	a, a, a, a, a, a, a, a, a,
+	b, b,
+	c, c,
+	d, d, d, d,
+	e, e, e, e, e, e, e, e, e, e, e, e, 
+	f, f,
+	g, g, g,
+	h, h,
+	i, i, i, i, i, i, i, i, i, 
+	j,
+	k, 
+	l, l, l, l,
+	m, m,
+	n, n, n, n, n, n,
+	o, o, o, o, o, o, o, o, 
+	p, p,
+	q, 
+	r, r, r, r, r, r,
+	s, s, s, s,
+	t, t, t, t, t, t,
+	u, u, u, u,
+	v, v,
+	w, w,
+	x, 
+	y, y,
+	z, 
+	blank, blank
+]
+
+var tileValues = {
+	a: 1,
+	b: 3,
+	c: 3,
+	d: 2,
+	e: 1,
+	f: 4,
+	g: 2,
+	h: 4,
+	i: 1,
+	j: 8,
+	k: 5,
+	l: 1,
+	m: 3,
+	n: 1,
+	o: 1,
+	p: 3,
+	q: 10,
+	r: 1,
+	s: 4,
+	t: 1,
+	u: 1,
+	v: 4,
+	w: 4,
+	x: 8,
+	y: 4,
+	z: 10,
+	blank: 0
+}
