99d8a8cef8731861ed657fb964332ea9ba0e7186
Will McVay <willmcvay@gmail.com>
2014-09-29 19:35:44 +0100

    Made a start on saving to mongodb

--- server/server.js
+++ server/server.js
@@ -1,31 +1,89 @@
-// DEPENDENCIES
-// ============
 var express = require("express"),
     http = require("http"),
     port = (process.env.PORT || 8001),
-    server = module.exports = express();
+    server = module.exports = express(),
+    path = require("path"),
+    mongoose = require('mongoose');
 
-// SERVER CONFIGURATION
-// ====================
+mongoose.connect('mongodb://localhost/my_database');
+
+var Game = mongoose.model('Game', new mongoose.Schema({
+	playerOne: {
+		score: Number,
+		tileRack: Array
+	},
+	playerTwo: {
+		score: Number,
+		tileRack: Array
+	},
+	tileBag: Array,
+	availableSquares: Array,
+	tiles: Array
+}));
+
 server.configure(function () {
 
     server.use(express["static"](__dirname + "/../public"));
 
     server.use(express.errorHandler({
-
         dumpExceptions:true,
-
         showStack:true
-
     }));
 
     server.use(server.router);
 });
 
-// SERVER
-// ======
+server.get('/api/games/:id', function(req, res){
+  return Game.findById(req.params.id, function(err, game) {
+    if (!err) {
+      return res.send(game);
+    }
+  });
+});
 
-// Start Node.js Server
+server.put('/api/games/:id', function(req, res){
+  return Game.findById(req.params.id, function(err, game) {
+    game.playerOne = req.body.playerOne;
+    game.playerTwo = req.body.playerTwo;
+    game.tileBag = req.body.tileBag;
+    game.availableSquares = req.body.availableSquares;
+    game.tiles = req.body.tiles;
+    return game.save(function(err) {
+      if (!err) {
+        console.log("updated");
+      }
+      return res.send(game);
+    });
+  });
+});
+
+server.post('/api/games', function(req, res){
+  var game;
+  game = new Game({
+    playerOne: req.body.playerOne,
+    playerTwo: req.body.playerTwo,
+    tileBag: req.body.tileBag,
+    availableSquares: req.body.availableSquares,
+    tiles: req.body.tiles
+  });
+  game.save(function(err) {
+    if (!err) {
+      return console.log("created");
+    }
+  });
+  return res.send(game);
+});
+
+server.delete('/api/games/:id', function(req, res){
+  return Game.findById(req.params.id, function(err, game) {
+    return game.remove(function(err) {
+      if (!err) {
+        console.log("removed");
+        return res.send('')
+      }
+    });
+  });
+});
+
 http.createServer(server).listen(port);
-
-console.log('Welcome to MRB-Lite!\n\nPlease go to http://localhost:' + port + ' to start using Marionette, Require.js and Backbone.js');
+console.log('Running on localhost:8001');
